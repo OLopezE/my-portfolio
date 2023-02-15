5eed95d142d4748469805b8142f02d0ba7f58970
Oscar francisco Lopez escobar <olopeze@outlook.com>
2023-02-14 21:14:56 -0600

    started with work experience

---
+++ src/js/api/workExperience/workExperience.js
@@ -0,0 +1,16 @@
+const workExperience = [
+  {
+    name: '100 Ladrillos',
+    jobTitle: 'Frontend software Engineer',
+    description: [
+      '',
+      '',
+    ],
+    picture: '',
+
+  },
+  {
+  }
+]
+
+export default workExperience;

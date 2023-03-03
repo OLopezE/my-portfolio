0f2a2c1770984319cf22167a8eb085f4baff8ad5
Oscar francisco Lopez escobar <olopeze@outlook.com>
2023-03-02 22:09:47 -0600

    modify contact

--- src/js/views/contact/Contact.jsx
+++ src/js/views/contact/Contact.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Contact() {
+const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <h1>Get in contact with me</h1>
+      <p>These are my socials</p>
+      <Link>Linkedin</Link>
+      <Link>Github</Link>
+    </div>
   )
 }
 

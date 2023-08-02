9e2b87ca0d6caf88922656920cbe226af615c21b
Oscar francisco Lopez escobar <olopeze@outlook.com>
2023-08-01 22:21:59 -0600

    adding mixins

---
+++ src/js/style/palette/palette.js
@@ -0,0 +1,13 @@
+const backgroundColor = '#030407;';
+const main = '#bb74d3;';
+const secondary = '#0f1024;';
+const complementary  = '#6249b6;';
+const textColor = '#f8eeee'
+
+export {
+  backgroundColor,
+  main,
+  secondary,
+  complementary,
+  textColor
+};

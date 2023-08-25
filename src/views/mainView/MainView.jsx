7c2b0a6b12a457b8567def60c46ca5b9ec72b63a
Oscar francisco Lopez escobar <olopeze@outlook.com>
2023-08-25 00:38:43 -0600

    Adding some text

--- src/views/mainView/MainView.jsx
+++ src/views/mainView/MainView.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import { MainViewSection } from './MainView.style';
 
 const MainView = () => {
   return (
-    <div>
+    <MainViewSection>
       <h1>Meet Oscar</h1>
-    </div>
+      <div>
+        <img />
+        <p>I am a 24 year old software engineer from Chihuahua, Mexico. React enjoyer, but also a fan of reading about good practices and design patterns.</p>
+      </div>
+    </MainViewSection>
   )
 }
 
---
+++ src/views/mainView/MainView.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const MainViewSection = styled.section`
+  position: relative;
+  min-height: 100svh;
+
+  > h1 {
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    font-size: 5rem;
+  }
+`;
+
+export {
+  MainViewSection
+}

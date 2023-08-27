ef91b0179a823c055cd59f4850f0eaaa456050b9
Oscar francisco Lopez escobar <olopeze@outlook.com>
2023-08-26 23:46:31 -0600

    changing title and meet me mobile

--- src/views/meetMe/MeetMe.style.js
+++ src/views/meetMe/MeetMe.style.js
@@ -1,19 +1,30 @@
 import styled from 'styled-components'
 
 const MeetMeSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
   padding: 0rem 3rem;
   text-align: justify;
 
+  > h2 {
+    padding: 4rem 0 1rem;
+    margin: 0;
+    font-size: 3rem;
+    text-align: center;
+  }
+
   @media (min-width: 800px) {
-    padding-top: 10rem;
-    display: flex;
     justify-content: space-around;
+    flex-direction: row;
 
     > div {
+      padding-top: 10rem;
       width: 300px;
     }
 
     > h2 {
+      padding-top: 10rem;
       margin: 0;
       font-size: 5rem;
     }

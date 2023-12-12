fd0ce22de91df3fd04ebffa2d325511d12957521
Oscar francisco Lopez escobar <olopeze@outlook.com>
2023-12-11 21:15:58 -0600

    calculating my age

--- src/views/meetMe/MeetMe.jsx
+++ src/views/meetMe/MeetMe.jsx
@@ -2,12 +2,21 @@ import { MeetMeSection } from './MeetMe.style';
 import myself from '/images/me_tequila.jpeg'
 
 const MeetMe = () => {
+  const getAge = today => {
+    let birthDate = new Date('11-18-1998');
+    var timeDiff = today - birthDate;
+
+    var age = Math.floor(timeDiff / (365.25 * 24 * 60 * 60 * 1000));
+
+    return age;
+  };
+
   return (
     <MeetMeSection>
       <h2>Meet Oscar</h2>
       <div>
         <img src={myself} alt="handsome Oscar Lopez at Tequila" />
-        <p>I am a 24 year old software engineer from Chihuahua, Mexico. React enjoyer, but also a fan of reading about good practices and design patterns.</p>
+        <p>I am a {getAge(new Date())} year old software engineer from Chihuahua, Mexico. React enjoyer, but also a fan of reading about good practices and design patterns.</p>
       </div>
     </MeetMeSection>
   )
